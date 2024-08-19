724ac059e382b393efce445617e2e84c1ea83633
HIMANSHU RANJAN <122608578+himanshuranjan977@users.noreply.github.com>
2024-08-19 17:09:35 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+  <strong>Copyright &copy; 2024 <a href="https://www.linkedin.com/in/himanshu-ranjan-6019a6215/">Himanshu Ranjan</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024 <a href="https://www.linkedin.com/in/himanshu-ranjan-6019a6215/">Himanshu Ranjan</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
